e367adaa31a021ab9ef527427c65b13b718c81a9
Fazliddindev2007 <afazliddin615@gmail.com>
2024-02-24 09:07:59 +0500

    amaily mashq

---
+++ index.py
@@ -0,0 +1,19 @@
+from turtle import *
+import colorsys 
+tracer(3)
+setup (700,900)
+h = 0.7
+bgcolor('black')
+pensize(2.3)
+
+for i in range(150)
+    c = colorsys.hsv_to_rgb(h,1,1)
+    color(c)
+    h+=0.004
+    circle(150-i,90)
+    
+    lt(90)
+    lt(20)
+    circle(150-i ,90)
+    lt(18)
+done()
